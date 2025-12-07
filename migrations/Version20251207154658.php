18b875754d5b568a550edbe765b1aa265696e718
Pavel Tyulnev <pash_tyul95@mail.ru>
2025-12-07 16:47:19 +0100

    Fix

--- migrations/Version20251207154658.php
+++ migrations/Version20251207154658.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20251207153838 extends AbstractMigration
+final class Version20251207154658 extends AbstractMigration
 {
     public function getDescription(): string
     {
